341ff34ec27ace19b889e333069e92365f7705d0
OmPatel1511 <202301027@daiict.ac.in>
2025-10-20 22:48:37 +0530

    Find Doctors page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import SignupPage from './pages/SignupPage';
 import PatientDashboard from './pages/PatientDashboard';
 import FindDoctorsPage from './pages/FindDoctorsPage';
 import BookAppointmentPage from './pages/BookAppointmentPage';
-
+import DoctorProfilePage from './pages/DoctorProfilePage';
 function App() {
  // This component now only contains the Routes.
  return (
@@ -15,10 +15,11 @@ function App() {
  <Route path="/" element={<LandingPage />} />
  <Route path="/login" element={<LoginPage />} />
  <Route path="/signup" element={<SignupPage />} />
- 
+
  <Route path="/patient/dashboard" element={<PatientDashboard />} />
  <Route path="/patient/doctors" element={<FindDoctorsPage />} />
 <Route path="/patient/book/:doctorId" element={<BookAppointmentPage />} />
+<Route path="/doctor/:id" element={<DoctorProfilePage />} />
  </Routes>
 );
 }
