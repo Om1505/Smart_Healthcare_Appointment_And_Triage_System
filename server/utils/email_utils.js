11558014a7ffb277348716e8bc0e995bd028175b
Om1505 <omp151105@gmail.com>
2025-11-20 16:14:32 +0530

    Small changes in mail parameters for deployment

--- server/utils/email_utils.js
+++ server/utils/email_utils.js
@@ -3,12 +3,16 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail', 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  host: 'smtp.gmail.com', 
+  port: 465,             
+  secure: true,     
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  
+  connectionTimeout: 10000, 
+});
 
   const mailOptions = {
     from: `IntelliConsult <${process.env.EMAIL_USER}>`,
